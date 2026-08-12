5d577a8b3ec3ed94c5b33ee636fa6cdf57089595
MilesSkol63 <s2400363@usls.edu.ph>
2026-08-12 10:29:45 +0800

    feat: add home page and 3 pages that links with each other (#3)
    * add main page and 3 pages with links to each other
    
    * add new tab on click for each link

---
+++ lab2/page1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <p>Page 1</p>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="page2.php" target="blank">Go to page 2</a></li>
+            <li><a href="page3.php" target="blank">Go to page 3</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
