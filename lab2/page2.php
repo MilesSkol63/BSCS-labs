7eb63c3a406c6ea5d979e5645c450ed351eabf16
MilesSkol63 <s2400363@usls.edu.ph>
2026-08-12 10:16:13 +0800

    add main page and 3 pages with links to each other

---
+++ lab2/page2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <p>Page 2</p>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="page1.php">Go to page 1</a></li>
+            <li><a href="page3.php">Go to page 3</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
---
+++ lab2/page3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <p>Page 3</p>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="page1.php">Go to page 1</a></li>
+            <li><a href="page2.php">Go to page 2</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
